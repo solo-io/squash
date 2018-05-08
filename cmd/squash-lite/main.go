258da92e5154564fa8ea821d16ddbf575ec608dd
Yuval Kohavi <yuval.kohavi@gmail.com>
2018-05-07 23:09:15 -0400

    more usage

--- cmd/squash-lite/main.go
+++ cmd/squash-lite/main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/solo-io/squash/pkg/lite/kube"
 )
 
+const descriptionUsage = `Normally squash lite requires no arguments. just run it!
+it works by creating additional debug pod and then attaching to it. 
+Kubernetes with CRI is needed. Due to a technical limitation, squash-lite doesn't support 
+scratch images at the moment (squash lite relys on the 'ls' command present in the image). 
+`
+
 func main() {
 	var cfg kube.SquashConfig
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "%s\n", descriptionUsage)
+		flag.PrintDefaults()
+	}
+
 	flag.BoolVar(&cfg.NoClean, "no-clean", false, "don't clean temporar pod when existing")
 	flag.BoolVar(&cfg.ChooseDebugger, "no-guess-debugger", false, "don't auto detect debugger to use")
 	flag.BoolVar(&cfg.ChoosePod, "no-guess-pod", false, "don't auto detect pod to use")
