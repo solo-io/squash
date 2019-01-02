d1fd91c9cad4196545ca8ea0d26d4dec68fb20e6
Mitch Kelley <mitch.kelley@solo.io>
2019-01-02 13:45:19 -0500

    add basic test utils

---
+++ test/util/client.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"context"
+
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients/factory"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients/kube"
+	"github.com/solo-io/solo-kit/pkg/utils/kubeutils"
+	"github.com/solo-io/squash/pkg/api/v1"
+)
+
+func GetDebugAttachmentClient(ctx context.Context) (*v1.DebugAttachmentClient, error) {
+	cfg, err := kubeutils.GetConfig("", "")
+	if err != nil {
+		return nil, err
+	}
+	cache := kube.NewKubeCache()
+	rcFactory := &factory.KubeResourceClientFactory{
+		Crd:         v1.DebugAttachmentCrd,
+		Cfg:         cfg,
+		SharedCache: cache,
+	}
+	client, err := v1.NewDebugAttachmentClient(rcFactory)
+	if err != nil {
+		return nil, err
+	}
+	if err := client.Register(); err != nil {
+		return nil, err
+	}
+	return &client, nil
+}
---
+++ test/util/client_test.go
@@ -0,0 +1,76 @@
+package util_test
+
+import (
+	"context"
+	"fmt"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	"github.com/solo-io/squash/pkg/api/v1"
+	"github.com/solo-io/squash/test/util"
+)
+
+func generateDebugAttachment(name, namespace, dbgger, image, pod, container, processName string) v1.DebugAttachment {
+
+	da := v1.DebugAttachment{
+		Metadata: core.Metadata{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Debugger:  dbgger,
+		Image:     image,
+		Pod:       pod,
+		Container: container,
+	}
+	if processName != "" {
+		da.ProcessName = processName
+	}
+	return da
+}
+func generateDebugAttachmentDlv1(name, namespace string) v1.DebugAttachment {
+	dbgger := "dlv"
+	image := "mk"
+	pod := "somepod"
+	container := "somecontainer"
+	processName := "pcsnm"
+	return generateDebugAttachment(name, namespace, dbgger, image, pod, container, processName)
+}
+
+var _ = Describe("utils", func() {
+	It("should generate debug attachment", func() {
+		ctx := context.TODO()
+		daClient, err := util.GetDebugAttachmentClient(ctx)
+		Expect(err).To(BeNil())
+
+		name := "aname2"
+		namespace := "squash"
+		da := generateDebugAttachmentDlv1(name, namespace)
+		writeOpts := clients.WriteOpts{
+			Ctx:               ctx,
+			OverwriteExisting: true,
+		}
+		written, err := (*daClient).Write(&da, writeOpts)
+		Expect(err).To(BeNil())
+		Expect(written.Metadata.Name).To(Equal(name))
+		Expect(written.Metadata.Namespace).To(Equal(namespace))
+
+		readOpts := clients.ReadOpts{
+			Ctx: ctx,
+		}
+		read, err := (*daClient).Read(namespace, name, readOpts)
+		Expect(err).To(BeNil())
+		Expect(read.Metadata.Name).To(Equal(name))
+		Expect(read.Metadata.Namespace).To(Equal(namespace))
+		fmt.Println(read)
+
+		// Cleanup
+		deleteOpts := clients.DeleteOpts{
+			Ctx:            ctx,
+			IgnoreNotExist: false,
+		}
+		err = (*daClient).Delete(namespace, name, deleteOpts)
+		Expect(err).To(BeNil())
+	})
+})
