921f4d914e6f652df8093d4063fc5c3af9de5219
Mitch Kelley <mitch.kelley@solo.io>
2019-01-03 16:22:48 -0500

    randomly generate the id

--- pkg/cmd/cli/debug_container.go
+++ pkg/cmd/cli/debug_container.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 
+	soloutils "github.com/solo-io/go-utils/v1/common"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	"github.com/solo-io/squash/pkg/api/v1"
@@ -23,39 +24,15 @@ type Error struct {
 }
 
 func DebugContainerCmd(opts *options.Options) *cobra.Command {
+	dcOpts := &opts.DebugContainer
 	var debugContainerCmd = &cobra.Command{
 		Use:   "debug-container image pod container [debugger]",
 		Short: "debug-container adds a container type debug config",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			var image, pod, container, debuggertype string
-			debuggertype = "gdb"
-			var err error
-			switch len(args) {
-			case 4:
-				debuggertype = args[3]
-				fallthrough
-			case 3:
-				image, pod, container = args[0], args[1], args[2]
-			default:
-				err = errors.New("invalid number of arguments")
-			}
-			if err != nil {
+			if err := ensureDebugContainerOpts(dcOpts, args); err != nil {
 				return err
 			}
-
-			da := v1.DebugAttachment{
-				Metadata: core.Metadata{
-					Name:      "tmpname111", // TODO(mitchdraft) - implement a naming scheme
-					Namespace: defaults.SquashNamespace,
-				},
-				Pod:            pod,
-				Container:      container,
-				ProcessName:    opts.ProcessName,
-				Image:          image,
-				Debugger:       debuggertype,
-				DebugNamespace: opts.Namespace,
-			}
-			fmt.Println("about to trigger")
+			da := debugAttachmentFromOpts(*dcOpts)
 			dbgattchment, err := debugContainer(da)
 			if err != nil {
 				return err
@@ -80,10 +57,45 @@ func DebugContainerCmd(opts *options.Options) *cobra.Command {
 		},
 	}
 
-	debugContainerCmd.Flags().StringVarP(&opts.Namespace, "namespace", "n", "default", "Namespace the pod belongs to")
-	debugContainerCmd.Flags().StringVarP(&opts.ProcessName, "processName", "p", "", "Process name to debug (defaults to the first running process)")
+	debugContainerCmd.Flags().StringVarP(&dcOpts.Namespace, "namespace", "n", "default", "Namespace the pod belongs to")
+	debugContainerCmd.Flags().StringVarP(&dcOpts.ProcessName, "processName", "p", "", "Process name to debug (defaults to the first running process)")
 
 	return debugContainerCmd
+}
+
+func ensureDebugContainerOpts(dcOpts *options.DebugContainer, args []string) error {
+	var err error
+	switch len(args) {
+	case 4:
+		dcOpts.DebuggerType = args[3]
+		fallthrough
+	case 3:
+		dcOpts.Image = args[0]
+		dcOpts.Pod = args[1]
+		dcOpts.Container = args[2]
+	default:
+		err = errors.New("invalid number of arguments")
+	}
+	if err != nil {
+		return err
+	}
+	dcOpts.Name = soloutils.RandStringBytes(6)
+	return nil
+}
+
+func debugAttachmentFromOpts(o options.DebugContainer) v1.DebugAttachment {
+	return v1.DebugAttachment{
+		Metadata: core.Metadata{
+			Name:      o.Name,
+			Namespace: defaults.SquashNamespace,
+		},
+		Pod:            o.Pod,
+		Container:      o.Container,
+		ProcessName:    o.ProcessName,
+		Image:          o.Image,
+		Debugger:       o.DebuggerType,
+		DebugNamespace: o.Namespace,
+	}
 }
 
 func debugContainer(da v1.DebugAttachment) (*v1.DebugAttachment, error) {
@@ -96,7 +108,6 @@ func debugContainer(da v1.DebugAttachment) (*v1.DebugAttachment, error) {
 		Ctx:               ctx,
 		OverwriteExisting: false,
 	}
-	fmt.Println("writing")
-	return (*daClient).Write(&da, writeOpts)
 
+	return (*daClient).Write(&da, writeOpts)
 }
--- pkg/cmd/cli/options/options.go
+++ pkg/cmd/cli/options/options.go
@@ -1,8 +1,17 @@
 package options
 
 type Options struct {
-	Url         string
-	Json        bool
-	Namespace   string
-	ProcessName string
+	Url            string
+	Json           bool
+	DebugContainer DebugContainer
+}
+
+type DebugContainer struct {
+	Name         string
+	Namespace    string
+	Image        string
+	Pod          string
+	Container    string
+	ProcessName  string
+	DebuggerType string
 }
