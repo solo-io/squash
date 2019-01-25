fdaf7823948660c09f0cb297a15ed9598cf1aa96
Mitch Kelley <mitch.kelley@solo.io>
2019-01-25 09:22:49 -0500

    comment the comment

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,5 +2,5 @@ package version
 
 var Version = "dev" // This will be overwritten by the linker
 
-DATE = $(shell date '+%Y-%m-%d.%H:%M:%S')
+// format: date '+%Y-%m-%d.%H:%M:%S'
 var TimeStamp = "2019-01-25.08:59:24" // This will be overwritten by the linker
