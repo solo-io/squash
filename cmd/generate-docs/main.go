62b1a3708e0da10e5a763bc7b68515193935bc2f
Mitch Kelley <mitch.kelley@solo.io>
2019-01-27 19:06:39 -0500

    basic cli docs and site gen

---
+++ cmd/generate-docs/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+	"github.com/solo-io/squash/pkg/cmd/cli"
+	"github.com/solo-io/squash/pkg/version"
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+func main() {
+	app, err := cli.App(version.Version)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	disableAutoGenTag(app)
+
+	err = doc.GenMarkdownTree(app, "./docs/cli")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func disableAutoGenTag(c *cobra.Command) {
+	c.DisableAutoGenTag = true
+	for _, c := range c.Commands() {
+		disableAutoGenTag(c)
+	}
+}
